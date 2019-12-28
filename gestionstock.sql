83605ae190bfc26837189895ebf9b6cbb47544cb
saadia <saadia.karray1996@gmail.com>
2019-12-28 12:09:05 +0100

    new base

--- gestionstock.sql
+++ gestionstock.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  sam. 07 déc. 2019 à 09:46
--- Version du serveur :  10.1.38-MariaDB
--- Version de PHP :  7.3.4
+-- Généré le :  sam. 28 déc. 2019 à 12:08
+-- Version du serveur :  10.4.6-MariaDB
+-- Version de PHP :  7.3.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -49,7 +49,8 @@ INSERT INTO `article` (`id`, `lib_article`, `prix_HT`, `TVA`, `qte_article`, `ph
 (129, 'article algesic', 9, 2, 10, 'pY0h3Bc5.png', 'medicament', 'A001', 0, 0),
 (130, 'AZERTYU', 0, 0, 0, '', '', '', 0, 0),
 (131, 'AZERTYU', 0, 0, 0, '', '', '', 0, 0),
-(134, '', 0, 0, 0, '', '', '', 0, 0);
+(134, '', 0, 0, 0, '', '', '', 0, 0),
+(135, 'article algesic', 9, 2, 10, '', 'medicaments', 'A001', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -91,13 +92,27 @@ INSERT INTO `categorie` (`id`, `lib_categorie`) VALUES
 CREATE TABLE `client` (
   `id` int(11) NOT NULL,
   `num_tel` varchar(8) NOT NULL,
-  `adr_client` int(50) NOT NULL,
+  `adr_client` varchar(50) NOT NULL,
   `type_id` char(1) NOT NULL,
-  `mat_fiscale` int(10) NOT NULL,
-  `mat_client` int(8) NOT NULL,
+  `mat_fiscale` varchar(10) NOT NULL,
+  `mat_client` varchar(8) NOT NULL,
   `nom_client` varchar(20) NOT NULL,
   `prenom_client` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `client`
+--
+
+INSERT INTO `client` (`id`, `num_tel`, `adr_client`, `type_id`, `mat_fiscale`, `mat_client`, `nom_client`, `prenom_client`) VALUES
+(22, '58741236', 'Route tunis km 10', '0', '', '11025684', 'Karray', 'Saadia'),
+(34, '56258789', 'hyyyy6675', '', '5828522', '', '', ''),
+(36, '56258789', 'BP 103 Merkez El Alia', '', '5885', '', 'Primatec', ''),
+(47, '58741236', 'Route manzel chaker km 2', '', '11897', '', 'FOD', ''),
+(50, '56879412', 'BP 103 Merkez El Alia', '1', '5828522', '', 'FOD', ''),
+(54, '7777', 'Route manzel chaker km 2', '0', '', '11025684', 'Karray', 'Saadia'),
+(55, '', '', '', '', '', '', ''),
+(56, '58741236', 'BP 103 Merkez El Alia', '1', '5828522', '', 'FOD', '');
 
 -- --------------------------------------------------------
 
@@ -106,11 +121,95 @@ CREATE TABLE `client` (
 --
 
 CREATE TABLE `commande` (
-  `id` int(11) NOT NULL,
+  `id_commande` int(11) NOT NULL,
   `date_commande` date NOT NULL,
   `adresse_livraison` varchar(100) NOT NULL,
-  `id_client` int(10) NOT NULL
+  `id_client` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `commande`
+--
+
+INSERT INTO `commande` (`id_commande`, `date_commande`, `adresse_livraison`, `id_client`) VALUES
+(1, '0000-00-00', '', '0'),
+(2, '0000-00-00', '', '0'),
+(3, '0000-00-00', '', '0'),
+(4, '0000-00-00', '', '0'),
+(5, '0000-00-00', '', '0'),
+(6, '0000-00-00', '', '0'),
+(7, '0000-00-00', '', '0'),
+(8, '0000-00-00', '', '0'),
+(9, '0000-00-00', '', '0'),
+(10, '0000-00-00', '', '0'),
+(11, '0000-00-00', '', '0'),
+(12, '0000-00-00', '', '0'),
+(13, '0000-00-00', '', '0'),
+(14, '0000-00-00', '', '0'),
+(15, '0000-00-00', '', '0'),
+(17, '0000-00-00', '', '0'),
+(18, '0000-00-00', '', '0'),
+(19, '0000-00-00', '', '0'),
+(20, '0000-00-00', '', '0'),
+(21, '0000-00-00', '', '0'),
+(22, '0000-00-00', '', '0'),
+(23, '0000-00-00', '', '0'),
+(24, '0000-00-00', '', '0'),
+(25, '0000-00-00', '', '0'),
+(26, '0000-00-00', '', '0'),
+(27, '0000-00-00', '', '0'),
+(28, '0000-00-00', '', '0'),
+(29, '0000-00-00', '', '0'),
+(30, '0000-00-00', '', '0'),
+(31, '0000-00-00', '', '0'),
+(32, '0000-00-00', '', '0'),
+(33, '0000-00-00', '', '0'),
+(34, '0000-00-00', '', '0'),
+(35, '0000-00-00', '', '0'),
+(36, '0000-00-00', '', '0'),
+(37, '0000-00-00', '', '0'),
+(38, '0000-00-00', '', '0'),
+(39, '0000-00-00', '', '0'),
+(40, '0000-00-00', '', '0'),
+(41, '0000-00-00', '', '0'),
+(42, '0000-00-00', '', '0'),
+(43, '0000-00-00', '', '0'),
+(44, '0000-00-00', '', '0'),
+(45, '0000-00-00', '', '0'),
+(46, '0000-00-00', '', '0'),
+(47, '0000-00-00', '', '0'),
+(48, '0000-00-00', '', '0'),
+(49, '0000-00-00', '', '0'),
+(50, '0000-00-00', '', '0'),
+(51, '0000-00-00', '', '0'),
+(52, '0000-00-00', '', '0'),
+(53, '0000-00-00', '', '0'),
+(54, '0000-00-00', '', '0'),
+(55, '0000-00-00', '', '0'),
+(56, '0000-00-00', '', '0'),
+(57, '0000-00-00', '', '0'),
+(58, '0000-00-00', '', '0'),
+(59, '0000-00-00', '', '0'),
+(60, '0000-00-00', '', '0'),
+(61, '0000-00-00', '', '0'),
+(62, '2019-12-07', 'route Tunis km 8', '0'),
+(63, '2019-12-07', 'route Tunis km 8', '0'),
+(64, '0000-00-00', '', '22'),
+(65, '0000-00-00', '', '22'),
+(66, '0000-00-00', '', '22'),
+(67, '2010-10-17', 'BP 103 Merkez El Alia, 40', '22'),
+(68, '2010-10-17', 'BP 103 Merkez El Alia, 40', '22'),
+(69, '2020-02-20', 'route Tunis km 8', '22'),
+(70, '2020-02-20', 'route Tunis km 8', '22'),
+(71, '2020-02-20', 'route Tunis km 8', '22'),
+(72, '2013-02-20', 'route Tunis km 8', '22'),
+(73, '2013-02-20', 'route Tunis km 8', '22'),
+(74, '2020-10-15', 'route gremda km 7', '22'),
+(75, '0000-00-00', '', ''),
+(76, '0000-00-00', '', ''),
+(77, '0000-00-00', '', ''),
+(78, '0000-00-00', '', ''),
+(79, '0000-00-00', '', '');
 
 -- --------------------------------------------------------
 
@@ -126,6 +225,31 @@ CREATE TABLE `details_commande` (
   `TVA` float NOT NULL,
   `id_commande` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `details_commande`
+--
+
+INSERT INTO `details_commande` (`id`, `qte_commande`, `prix_unitaire`, `taux_remise_accorde`, `TVA`, `id_commande`) VALUES
+(1, 0, 0, 0, 0, 0),
+(2, 0, 0, 0, 0, 0),
+(3, 0, 0, 0, 0, 0),
+(4, 0, 0, 0, 0, 0),
+(5, 0, 0, 0, 0, 0),
+(6, 0, 0, 0, 0, 0),
+(7, 0, 0, 0, 0, 0),
+(8, 20, 2, 0, 0, 0),
+(9, 60, 56, 0, 0, 0),
+(10, 60, 56, 0, 0, 0),
+(11, 60, 56, 0, 0, 0),
+(12, 15, 9, 2, 1, 0),
+(13, 15, 9, 2, 1, 0),
+(14, 60, 20, 20, 3, 22),
+(15, 0, 0, 0, 0, 0),
+(16, 0, 0, 0, 0, 0),
+(17, 0, 0, 0, 0, 0),
+(18, 0, 0, 0, 0, 0),
+(19, 0, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -216,7 +340,7 @@ ALTER TABLE `client`
 -- Index pour la table `commande`
 --
 ALTER TABLE `commande`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_commande`);
 
 --
 -- Index pour la table `details_commande`
@@ -250,7 +374,7 @@ ALTER TABLE `remise`
 -- AUTO_INCREMENT pour la table `article`
 --
 ALTER TABLE `article`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=135;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
 
 --
 -- AUTO_INCREMENT pour la table `bon_livraison`
@@ -268,19 +392,19 @@ ALTER TABLE `categorie`
 -- AUTO_INCREMENT pour la table `client`
 --
 ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT pour la table `commande`
 --
 ALTER TABLE `commande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_commande` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=80;
 
 --
 -- AUTO_INCREMENT pour la table `details_commande`
 --
 ALTER TABLE `details_commande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT pour la table `facture`
