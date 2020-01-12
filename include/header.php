94ed48267d375a26726441b6c167a6a7a4c9f086
saadia <saadia.karray1996@gmail.com>
2020-01-12 11:26:24 +0100

    logout

--- include/header.php
+++ include/header.php
@@ -36,7 +36,7 @@
 						<ul class="dropdown-menu" role="menu">
 							<li><a href="#"><svg class="glyph stroked male-user"><use xlink:href="#stroked-male-user"></use></svg> Profile</a></li>
 							<li><a href="#"><svg class="glyph stroked gear"><use xlink:href="#stroked-gear"></use></svg> Settings</a></li>
-							<li><a href="#"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
+							<li><a  href="index.controller.php?action=logout&controller=admin"><svg class="glyph stroked cancel"><use xlink:href="#stroked-cancel"></use></svg> Logout</a></li>
 						</ul>
 					</li>
 				</ul>
